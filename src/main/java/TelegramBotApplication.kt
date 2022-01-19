46829603fbf0e1dc9bbb8a591a6f95dba75a4e5e
me <asp424@ya.ru>
2022-01-19 20:36:25 +0300

    Initial commit

---
+++ src/main/java/TelegramBotApplication.kt
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
+
+fun main() {
+    TelegramBotsApi(DefaultBotSession::class.java).registerBot(object: TelegramLongPollingBot() {
+        override fun getBotToken(): String  = "5008153892:AAHvn7MiDFxZLbOBioNnTqt0DNli0WQ1eKI"
+        override fun getBotUsername(): String = "Her"
+        override fun onUpdateReceived(update: Update?) {
+            execute(SendMessage(update?.message?.chatId.toString(), "ass"))
+            println(update?.message?.text)
+
+        }
+    })
+}
+
+
